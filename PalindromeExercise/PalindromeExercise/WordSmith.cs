66081d41b147273730eed31d2dbae4bc8f9e6031
Adam Measel <ameasel55@gmail.com>
2023-02-28 08:44:15 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using PalindromeExercise;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public static bool IsAPalindrome(string word)
+    {
+        string reversedWord = "";
+
+        for (int i = word.Length-1; i >= 0; i--)
+        {
+            reversedWord += word[i];
+        }
+        return (word.ToLower() == reversedWord.ToLower());
+        
+    }
+}
+
+ 
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]   
+        [InlineData("Kayak", true)]
+        [InlineData("excalibur", false)]
+        [InlineData("rotator", true)]
+        [InlineData("ottoman", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //arange
 
+            //act
+            var actual = WordSmith.IsAPalindrome(word);
+
+            //assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
